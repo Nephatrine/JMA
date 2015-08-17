0ac675664aa4a3d60a8f5a713c2da98209ffd78a
Daniel Wolf <nephatrine@gmail.com>
2015-08-17 00:05:16 -0400

    link in libepoxy

--- Source/Client/Main.cpp
+++ Source/Client/Main.cpp
@@ -1,6 +1,9 @@
+#include <epoxy/gl.h>
+#include <epoxy/wgl.h>
 #include <GLFW/glfw3.h>
 #include <stdlib.h>
 #include <stdio.h>
+
 static void error_callback(int error, const char* description)
 {
     fputs(description, stderr);
