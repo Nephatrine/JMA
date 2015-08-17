42e6fc1ccb36c8d57aab49d17a9c0d2fdce53183
Daniel Wolf <nephatrine@gmail.com>
2015-08-17 12:49:29 -0400

    updated vc12 solution for libepoxy

--- Source/Client/Main.cpp
+++ Source/Client/Main.cpp
@@ -1,18 +1,25 @@
+// Epoxy Headers
 #include <epoxy/gl.h>
 #include <epoxy/wgl.h>
+
+// GLFW Headers
 #include <GLFW/glfw3.h>
-#include <stdlib.h>
-#include <stdio.h>
+
+// C Library Headers
+#include <cstdlib>
+#include <cstdio>
 
 static void error_callback(int error, const char* description)
 {
     fputs(description, stderr);
 }
+
 static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
         glfwSetWindowShouldClose(window, GL_TRUE);
 }
+
 int main(void)
 {
     GLFWwindow* window;
